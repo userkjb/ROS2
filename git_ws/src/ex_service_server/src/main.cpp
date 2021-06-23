b61027e586ecfc65422fc9ef9643ba583628078f
kjb <kjb19830@rasteck.co.kr>
2021-06-23 17:17:53 +0900

    update tab

--- git_ws/src/ex_service_server/src/main.cpp
+++ git_ws/src/ex_service_server/src/main.cpp
@@ -3,11 +3,11 @@
 
 int main(int argc, char* argv[])
 {
-    rclcpp::init(argc, argv);
-    auto node = std::make_shared<ExServiceServer>();
-    rclcpp::spin(node);
-    rclcpp::shutdown();
-    return 0;
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<ExServiceServer>();
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
 }
 
 /*
