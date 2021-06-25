360f873e43f52715271f791955b2d5596b342378
kjb <kjb19830@rasteck.co.kr>
2021-06-25 11:46:09 +0900

    create service client

--- git_ws/src/ex_service_client/src/ExServiceClient.cpp
+++ git_ws/src/ex_service_client/src/ExServiceClient.cpp
@@ -1,9 +1,48 @@
 #include "ex_service_client/ExServiceClient.hpp"
 
-
-ExServiceClient::ExServiceClient() : Node("ex_service_client")
+ExServiceClient::ExServiceClient(const rclcpp::NodeOptions& node_options)
+  : Node("ex_service_client", node_options)
 {
+  std::cout << "111" << std::endl;
 
+  handle_client_ = this->create_client<action_msg_srv::srv::LogFileInfo>("log_service");
+
+  std::cout << "222" << std::endl;
+  timer_ = this->create_wall_timer(1s, std::bind(&ExServiceClient::timer_function, this));
+  while(!handle_client_->wait_for_service(1s))
+  {
+    if(!rclcpp::ok())
+    {
+      RCLCPP_ERROR(this->get_logger(), "Interrupted while waiting for the service.");
+      return;
+    }
+    RCLCPP_INFO(this->get_logger(), "Service not available, waiting again...");
+  }
 }
 
-ExServiceClient::~ExServiceClient(){}
+ExServiceClient::~ExServiceClient()
+{
+  std::cout << "end" << std::endl;
+}
+
+void ExServiceClient::timer_function()
+{
+  send_request();
+}
+
+
+void ExServiceClient::send_request()
+{
+  auto request = std::make_shared<LogSRV::Request>();
+
+
+  using ServiceResponseFuture = rclcpp::Client<LogSRV>::SharedFuture;
+  auto response_received_callback = [this](ServiceResponseFuture future){
+    auto response = future.get();
+    RCLCPP_INFO(this->get_logger(), "Result base_path : %s", response->base_path);
+    RCLCPP_INFO(this->get_logger(), "Result file_name : %s", response->file_name);
+    return;
+  };
+
+  auto future_result = handle_client_->async_send_request(request, response_received_callback);
+}
