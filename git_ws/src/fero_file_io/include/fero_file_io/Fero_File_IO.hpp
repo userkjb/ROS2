b1ba3fa0b521dbbc992094f1999f469cc4978ecc
kjb <kjb19830@rasteck.co.kr>
2021-06-25 14:42:42 +0900

    Add Timer, Publisher, Server

--- git_ws/src/fero_file_io/include/fero_file_io/Fero_File_IO.hpp
+++ git_ws/src/fero_file_io/include/fero_file_io/Fero_File_IO.hpp
@@ -2,6 +2,12 @@
 #define ARITHMETIC__FERO_FILE_IO_HPP_
 
 #include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "action_msg_srv/srv/log_file_info.hpp"
+
+#include <dirent.h>
+
+using namespace std::chrono_literals; // 1s
 
 class Fero_File_IO : public rclcpp::Node
 {
@@ -9,6 +15,25 @@ public :
   Fero_File_IO();
   virtual ~Fero_File_IO();
 private :
+  // booting
+  void Service_Function();
+
+  // Publisher Timer
+  void Pub_Timer();
+
+  // Service Client
+  void Create_Service();
+  void Read_file();
+private :
+  // Publisher Timer
+  rclcpp::TimerBase::SharedPtr pub_timer_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr fero_log_;
+
+  // Service Server
+  rclcpp::Service<action_msg_srv::srv::LogFileInfo>::SharedPtr log_file_server_;
+
+protected :
+  const std::string log_dir_ = "/home/fero/catkin_ws/log/....";
 };
 
 
--- git_ws/src/fero_file_io/src/Fero_File_IO.cpp
+++ git_ws/src/fero_file_io/src/Fero_File_IO.cpp
@@ -1,9 +1,56 @@
 #include "fero_file_io/Fero_File_IO.hpp"
 
+using std::placeholders::_1;
+
 Fero_File_IO::Fero_File_IO() : Node("fero_file_io")
 {
+  pub_timer_ = this->create_wall_timer(1s, std::bind(&Fero_File_IO::Pub_Timer, this));
 
+  rclcpp::QoS qos_profile = rclcpp::QoS(rclcpp::KeepLast(10));
+  fero_log_ = this->create_publisher<std_msgs::msg::String>("logTopic", qos_profile);
+
+  Create_Service();
 }
 
 Fero_File_IO::~Fero_File_IO()
-{}
+{
+  RCLCPP_INFO(this->get_logger(), "End Fero_File_IO Node");
+}
+
+// Service =========================================================
+void Fero_File_IO::Create_Service()
+{
+  auto recv_request = [this](
+    const std::shared_ptr<action_msg_srv::srv::LogFileInfo::Request> request,
+    std::shared_ptr<action_msg_srv::srv::LogFileInfo::Response> response) -> void
+    {
+      if(request == NULL){
+        std::cout << "not commant" << std::endl;
+      }
+      response->base_path = log_dir_;
+      response->file_name = "";
+    };
+
+  log_file_server_ = this->create_service<action_msg_srv::srv::LogFileInfo>("logFile_service", recv_request);
+}
+
+void Fero_File_IO::Read_file()
+{
+  DIR* dirp = opendir(log_dir_.c_str());
+  struct dirent* dp;
+
+  while((dp = readdir(dirp)) != NULL)
+  {
+    // dp->d_name
+  }
+  closedir(dirp);
+}
+
+
+// Timer =========================================================
+// Log Publisher Timer(1s)
+void Fero_File_IO::Pub_Timer()
+{
+
+  // fero_log_->publish();
+}
