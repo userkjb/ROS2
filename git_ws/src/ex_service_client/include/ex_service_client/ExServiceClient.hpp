98ae6873884b324178c54cb2a9d0bd4cd3dfebf6
kjb <kjb19830@rasteck.co.kr>
2021-06-25 11:45:50 +0900

    create headers

--- git_ws/src/ex_service_client/include/ex_service_client/ExServiceClient.hpp
+++ git_ws/src/ex_service_client/include/ex_service_client/ExServiceClient.hpp
@@ -1,14 +1,32 @@
 #ifndef ARITHMETIC__EX_SERVICE_CLIENT_HPP_
 #define ARITHMETIC__EX_SERVICE_CLIENT_HPP_
 
+#include <chrono>
+#include <memory>
+#include <string>
+#include <utility>
+
 #include "rclcpp/rclcpp.hpp"
+#include "action_msg_srv/srv/log_file_info.hpp"
+
+
+using namespace std::chrono_literals; // 1s
 
 class ExServiceClient : public rclcpp::Node
 {
 public :
-  ExServiceClient();
+  using LogSRV = action_msg_srv::srv::LogFileInfo;
+
+  explicit ExServiceClient(const rclcpp::NodeOptions& node_options = rclcpp::NodeOptions());
   virtual ~ExServiceClient();
+
 private :
+  void send_request();
+  void timer_function();
+private :
+  rclcpp::TimerBase::SharedPtr timer_;
+
+  rclcpp::Client<action_msg_srv::srv::LogFileInfo>::SharedPtr handle_client_;
 };
 
 
