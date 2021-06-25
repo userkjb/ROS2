4e93e506c5c262b5ae33d67cc09d5bef778e4add
kjb <kjb19830@rasteck.co.kr>
2021-06-25 11:44:52 +0900

    Server create

--- git_ws/src/ex_service_server/src/ExServiceServer.cpp
+++ git_ws/src/ex_service_server/src/ExServiceServer.cpp
@@ -2,7 +2,24 @@
 
 ExServiceServer::ExServiceServer() : Node("ex_service_server")
 {
+  std::cout << "111" << std::endl;
+  response_str_base_path_ = "path";
+  response_str_file_name_ = "file";
+  auto recv_request = [this](
+    const std::shared_ptr<LogSRV::Request> request,
+    std::shared_ptr<LogSRV::Response> response) -> void
+    {
+      // response_str_base_path_ = request-> ;
+      // response_str_file_name_ = request-> ;
+      if(request == NULL) std::cout << "null" << std::endl;
+      response->base_path = response_str_base_path_;
+      response->file_name = response_str_file_name_;
 
+      RCLCPP_INFO(this->get_logger(), "base_path = %s", response->base_path);
+      RCLCPP_INFO(this->get_logger(), "file_name = %s", response->file_name);
+    };
+
+  handle_server_ = create_service<LogSRV>("log_service", recv_request);
 }
 
 ExServiceServer::~ExServiceServer()
