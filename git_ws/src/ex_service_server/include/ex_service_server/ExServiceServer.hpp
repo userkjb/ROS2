3f1e6133b3bf3c2f3de2c070f16243ca355c2eeb
kjb <kjb19830@rasteck.co.kr>
2021-06-25 11:44:37 +0900

    Add Server header, using srv, send variable

--- git_ws/src/ex_service_server/include/ex_service_server/ExServiceServer.hpp
+++ git_ws/src/ex_service_server/include/ex_service_server/ExServiceServer.hpp
@@ -2,13 +2,25 @@
 #define ARITHMETIC__EX_SERVICE_SERVER_HPP_
 
 #include "rclcpp/rclcpp.hpp"
+#include "action_msg_srv/srv/log_file_info.hpp"
 
 class ExServiceServer : public rclcpp::Node
 {
 public :
   ExServiceServer();
   virtual ~ExServiceServer();
+
+  using LogSRV = action_msg_srv::srv::LogFileInfo;
+
 private :
+  // Callback Function
+  // double RecvCallBack();
+  // void SendCallBack();
+
+private :
+  std::string response_str_base_path_;
+  std::string response_str_file_name_;
+  rclcpp::Service<LogSRV>::SharedPtr handle_server_;
 };
 
 
